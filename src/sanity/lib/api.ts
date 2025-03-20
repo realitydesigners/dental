beaae6dbe1cdf7b57d76e2db0eaaebf54b276a90
Raymond <raymond.luke.spartz@gmail.com>
2025-03-20 09:39:24 -0500

    motion

--- src/sanity/lib/api.ts
+++ src/sanity/lib/api.ts
@@ -10,7 +10,7 @@ export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET ?? "production";
 
 export const projectId = assertValue(
   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  "Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID",
+  "Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID"
 );
 
 export const presentationOriginUrl = process.env.SANITY_STUDIO_PRESENTATION_URL;
@@ -18,7 +18,6 @@ export const presentationOriginUrl = process.env.SANITY_STUDIO_PRESENTATION_URL;
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-10-28";
 
-export const studioUrl =
-  process.env.NEXT_PUBLIC_SANITY_STUDIO_URL || "http://localhost:3000";
+export const studioUrl = process.env.NEXT_PUBLIC_SANITY_STUDIO_URL;
 
 export const title = process.env.SANITY_STUDIO_TITLE || "My Studio";
